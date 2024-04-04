f099342ce16cfb84997c481dfb651cd5b2c1c483
Nataliia Lisova <nglisova@gmail.com>
2024-04-04 17:52:18 +0300

    feature: test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center place-content-center  p-24">
             <h1 className="text-2xl md:text-4xl font-semibold text-primary-color">Lisova Codes</h1>
             <h2 className="md:text-xl mt-6 text-complementary-color">Personal blog</h2>
-            <p className="mt-36 text-complementary-color">Coming soon...</p>
+            <p className="mt-36 text-complementary-color">Coming soon</p>
         </main>
     );
 }
