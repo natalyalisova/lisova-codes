8dea3a7753d246ae18afb9cab833759e0e5cc475
Nataliia Lisova <nglisova@gmail.com>
2024-04-04 16:54:56 +0300

    feature: initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
+        colors: {
+          "primary-color": "#7D23DC",
+          "primary-color-pail": "#974FE3",
+          "complementary-color": "#82DC23",
+          "complementary-color-pail": "#9BE34F",
+          "additional-color":"#FFAD00"
+        },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
