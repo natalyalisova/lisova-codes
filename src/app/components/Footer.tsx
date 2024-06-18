5a8dfb9fe03452d789ae00c7f74305e803aa67d9
Nataliia Lisova <nglisova@gmail.com>
2024-06-18 13:07:28 +0300

    feature: dark and light mode (#2)

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,8 +5,8 @@ const Footer = () => {
     const theme = useTheme();
     return (
         <ThemeProvider theme={theme}>
-            <div className="flex flex-row align-baseline items-center px-24 pb-16 text-sm">
-                <div>2024 &#169; By Nataliia Lisova</div>
+            <div className="flex flex-row align-baseline items-center p-6 text-sm">
+                <div className="dark:text-white">2024 &#169; By Nataliia Lisova</div>
             </div>
         </ThemeProvider>
     )
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,9 +1,9 @@
 "use client"
 import * as React from 'react';
-import {useContext, useState} from 'react';
+import {useEffect, useState} from 'react';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
-import {ThemeProvider, useTheme} from '@mui/material/styles';
+import {useTheme} from 'next-themes';
 import Image from "next/image";
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -18,10 +18,6 @@ import MenuItem from '@mui/material/MenuItem';
 import {Typography} from "@mui/material";
 import Link from "next/link";
 import {NavLinks} from "@/app/constants";
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import {ColorModeContext} from "@/app/components/ColorModeContext";
-import {violetMain} from "@/theme";
 
 
 function ResponsiveAppBar() {
@@ -35,140 +31,154 @@ function ResponsiveAppBar() {
         setAnchorElNav(null);
     };
 
-    const theme = useTheme();
-    const colorMode = useContext(ColorModeContext);
+    const {systemTheme, theme, setTheme} = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) return null;
+    const currentTheme = theme === 'system' ? systemTheme : theme;
 
     return (
-        <ThemeProvider theme={theme}>
-            <AppBar position="fixed">
-                <Container maxWidth="xl">
-                    <Toolbar disableGutters className="px-4">
-                        <Typography
-                            variant="h6"
-                            noWrap
-                            component="a"
-                            href="/"
+        <AppBar position="fixed" className="bg-white dark:bg-black">
+            <Container maxWidth="xl" className="mx-auto">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            ml: 12,
+                            mr: 2,
+                            display: {xs: 'none', md: 'flex'},
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            textDecoration: 'none',
+                        }}
+                        className="text-primary-color"
+                    >
+                        Lisova Codes
+                    </Typography>
+                    <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon className="text-primary-color"/>
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
                             sx={{
-                                mx: 2,
-                                display: {xs: 'none', md: 'flex'},
-                                fontFamily: 'monospace',
-                                fontWeight: 700,
-                                textColor: violetMain.main,
-                                textDecoration: 'none',
+                                display: {xs: 'block', md: 'none'},
                             }}
                         >
-                            Lisova Codes
-                        </Typography>
-                        <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
-                            <IconButton
-                                size="large"
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleOpenNavMenu}
-                                color="inherit"
-                            >
-                                <MenuIcon sx={{color: 'violet.main'}}/>
-                            </IconButton>
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorElNav}
-                                anchorOrigin={{
-                                    vertical: 'bottom',
-                                    horizontal: 'left',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'left',
-                                }}
-                                open={Boolean(anchorElNav)}
-                                onClose={handleCloseNavMenu}
-                                sx={{
-                                    display: {xs: 'block', md: 'none'},
-                                }}
-                            >
-                                {NavLinks.map((page) => (
-                                    <MenuItem key={page.key} onClick={handleCloseNavMenu}>
-                                        <Link
-                                            href={page.href}
-                                            target={page.target}
-                                            className="block hover:text-primary-color-pail mx-6"
-                                            aria-current="page">
-                                            {page.text}
-                                        </Link>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Box>
-                        <Typography
-                            variant="h5"
-                            noWrap
-                            component="a"
-                            href="/"
-                            sx={{
-                                mr: 2,
-                                display: {xs: 'flex', md: 'none'},
-                                flexGrow: 1,
-                                fontFamily: 'monospace',
-                                fontWeight: 700,
-                                color: 'violet.main',
-                                textDecoration: 'none',
-                            }}
-                        >
-                            Lisova Codes
-                        </Typography>
-                        <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
                             {NavLinks.map((page) => (
-                                <Button
-                                    key={page.key}
-                                    onClick={handleCloseNavMenu}
-                                    sx={{my: 2, display: 'block'}}
-                                >
+                                <MenuItem key={page.key} onClick={handleCloseNavMenu}>
                                     <Link
                                         href={page.href}
                                         target={page.target}
-                                        className="block hover:text-primary-color-pail mt-1"
+                                        className="block text-primary-color hover:text-primary-color-pail mx-6"
                                         aria-current="page">
                                         {page.text}
                                     </Link>
-                                </Button>
+                                </MenuItem>
                             ))}
-                        </Box>
-                        <Box sx={{flexGrow: 0, display: 'inline-flex'}}>
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            display: {xs: 'flex', md: 'none'},
+                            flexGrow: 1,
+                            fontSize: 20,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            textDecoration: 'none',
+                        }}
+                        className="text-primary-color"
+                    >
+                        Lisova Codes
+                    </Typography>
+                    <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+                        {NavLinks.map((page) => (
+                            <Button
+                                key={page.key}
+                                onClick={handleCloseNavMenu}
+                                sx={{my: 2, display: 'block'}}
+                            >
+                                <Link
+                                    href={page.href}
+                                    target={page.target}
+                                    className="block text-primary-color hover:text-primary-color-pail mt-1 dark:bg-black"
+                                    aria-current="page">
+                                    {page.text}
+                                </Link>
+                            </Button>
+                        ))}
+                    </Box>
+                    <Box sx={{flexGrow: 0, display: 'inline-flex'}}>
+                        <div className="flex justify-center mr-1">
+                            {currentTheme === 'dark' ? (
+                                <button
+                                    onClick={() => setTheme('light')}
+                                >
+                                    <Image src="/images/sun.svg" alt="logo" height={26} width={26}/>
+                                </button>
+                            ) : (
+                                <button
+                                    onClick={() => setTheme('dark')}
+                                >
+                                    <Image src="/images/moon.svg" alt="logo" height={26} width={26}/>
+                                </button>
+                            )}
+                        </div>
 
-                            <span className="mr-2">
-                                <IconButton sx={{ml: 1}} onClick={colorMode.toggleColorMode} color="inherit"
-                                            className="ml-2">
-                                 {theme.palette.mode === 'dark' ? <Brightness7Icon/> : <Brightness4Icon/>}
-                                 </IconButton>
-                            </span>
-
-                            <Tooltip title="GitHub" className="mr-2">
-                                <Link href="https://github.com/natalyalisova" target="_blank" rel="noopener noreferrer">
-                                    <GitHubIcon/>
-                                </Link>
-                            </Tooltip>
-                            <Tooltip title="LinkedIn" className="mr-2">
-                                <Link href="https://www.linkedin.com/in/nlisova/" target="_blank"
-                                      rel="noopener noreferrer">
-                                    <LinkedInIcon/>
-                                </Link>
-                            </Tooltip>
-                            <Tooltip title="Mastodon" className="mr-2">
-                                <Link href="https://hachyderm.io/@Lisova" target="_blank" rel="me">
-                                    <Image src={"/images/mastodon-icon.svg"}
-                                           width={26}
-                                           height={26}
-                                           priority={false}
-                                           alt={"Follow me on mastodon"}/>
-                                </Link>
-                            </Tooltip>
-                        </Box>
-                    </Toolbar>
-                </Container>
-            </AppBar>
-        </ThemeProvider>
+                        <Tooltip title="GitHub" className="mr-1">
+                            <Link href="https://github.com/natalyalisova" target="_blank" rel="noopener noreferrer">
+                                <GitHubIcon className="text-primary-color"/>
+                            </Link>
+                        </Tooltip>
+                        <Tooltip title="LinkedIn" className="mr-1">
+                            <Link href="https://www.linkedin.com/in/nlisova/" target="_blank"
+                                  rel="noopener noreferrer">
+                                <LinkedInIcon className="text-primary-color"/>
+                            </Link>
+                        </Tooltip>
+                        <Tooltip title="Mastodon">
+                            <Link href="https://hachyderm.io/@Lisova" target="_blank" rel="me">
+                                <Image src={"/images/mastodon-icon.svg"}
+                                       width={26}
+                                       height={26}
+                                       priority={false}
+                                       alt={"Follow me on mastodon"}/>
+                            </Link>
+                        </Tooltip>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 }
 
