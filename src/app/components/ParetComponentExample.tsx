2dca4fad51ab01c44c522bd7ac080bd75d04a04c
Nataliia Lisova <nglisova@gmail.com>
2024-10-08 22:10:38 +0300

    update:practice transfer data from child component to parent

--- src/app/components/ParetComponentExample.tsx
+++ src/app/components/ParetComponentExample.tsx
@@ -4,19 +4,30 @@ import Typography from "@mui/material/Typography";
 import ChildComponentExample from "@/app/components/ChildComponentExample";
 
 const ParentComponentExample = () => {
-    const [bgColor, setBgColor] = useState('bg-background-color-dark');
+    const [bgColor, setBgColor] = useState('dark:bg-black');
 
     const handleColorChange = (newColor: string) => {
         setBgColor(newColor);
     };
 
     return (
-        <div className={`flex-col text-center  ${bgColor} border-2 border-primary-color-pail p-5`}>
-            <Typography variant="subtitle1" className="dark:text-white mb-10">
+        <div className={`max-w-screen-lg flex-col text-center ${bgColor} border-2 border-primary-color-pail p-5 dark:text-white`}>
+            <h1 className="font-semibold mt-5">
                 Parent component
-            </Typography>
-            <div className={`w-full h-1/4 text-center mx-auto p-10`}>
+            </h1>
+            <h2 className="text-2xl font-bold my-3">State lifting</h2>
+            <div className={`w-full h-1/4 text-start mx-auto px-10 py-3`}>
                 <h2>This example demonstrates how to select a color in a child component for use in the parent component.</h2>
+                <p> When both the parent and child need to share and manage the same piece of state.<br/>
+
+                   <b>Why it’s good:</b> <br/>
+
+                    •	Shared control: Both the parent and child have access to the same state, so they can synchronize actions and updates.<br/>
+                    •	One source of truth: Since the state is lifted to the parent, both components are working with the same state, reducing bugs caused by multiple sources of truth.<br/>
+
+                   <b>Why not:</b> <br/>
+
+                    •	Prop drilling: As with callback functions, state lifting can result in passing props down through multiple layers of components, making the code less maintainable.</p>
             </div>
             <ChildComponentExample onColorChange={handleColorChange} />
         </div>
--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -4,7 +4,7 @@ import ParetComponentExample from "@/app/components/ParetComponentExample";
 
 export default function PracticePage() {
     return (
-        <Container maxWidth="xl">
+        <Container maxWidth="xl" className="mt-24">
             <div className="flex flex-col min-h-full items-center justify-center dark:bg-black">
                 <Typography variant="h1" gutterBottom sx={{
                     fontSize: {
